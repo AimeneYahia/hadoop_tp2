c118f9f618ed3082162613ebb1f6656600c6299e
Aïmène Yahia <ya.aim@hotmail.fr>
2016-10-14 11:23:56 +0200

    Question 3 (unfinished)

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ FirstNameByOrigin: Count first name by origin
 ## Question 2
 
 ByNbOfOrigin: Count number of first name by number of origin 
+
+## Question 3
+
+ProportionByGender: Proportion (in%) of male or female (unfinished)
---
+++ src/main/java/ProportionByGender.java
@@ -0,0 +1,70 @@
+/**
+ * Created by Aïmène on 14/10/2016.
+ */
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ProportionByGender {
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            // Input to String array (after split with ";")
+            // [] of row is the gender
+            // Standardize cases of both genders (separator can be "," or ", ")
+            // Split with ","
+            String[] genders = value.toString().split(";")[1].replaceAll(", ", ",").split(",");
+
+            // Write
+            for (String gender:
+                    genders) {
+                word.set(gender);
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "question 3");
+        job.setJarByClass(ProportionByGender.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}

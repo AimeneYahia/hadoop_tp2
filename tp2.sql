fd4d57d2fce24b2b348c684a707cb78b41270c91
Aïmène Yahia <ya.aim@hotmail.fr>
2016-10-21 13:39:22 +0200

    SQL request 1 fixed

--- tp2.sql
+++ tp2.sql
@@ -3,14 +3,18 @@ CREATE DATABASE ayahia LOCATION 'user/ayahia/db';
 
 -- creating a table 
 -- if there is a space after a comma, it will be found in the string (ie: ["french", " latin"])
-CREATE EXTERNAL TABLE ayahia.prenoms ( prenom STRING, gender ARRAY<STRING>,origin ARRAY<STRING>,version DOUBLE)ROW FORMAT 
+CREATE EXTERNAL TABLE ayahia.prenoms (prenom STRING, gender ARRAY<STRING>,origin ARRAY<STRING>,version DOUBLE)ROW FORMAT 
 	DELIMITED FIELDS TERMINATED BY '\073' COLLECTION ITEMS TERMINATED by ',' 
 	STORED AS TEXTFILE LOCATION '/user/ayahia/prenoms';
 	
 -- question 1
--- here origin is a table, each origin is not extracted yet
-SELECT origin, COUNT(prenom) FROM prenoms GROUP BY origin;
+-- issue not fixed: "origin" and " origin" are considered distinct
+-- how to use lateral view explode? example found here:
+--    https://cwiki.apache.org/confluence/display/Hive/LanguageManual+LateralView
+SELECT origins, COUNT(prenom)
+FROM prenoms 
+LATERAL VIEW EXPLODE (origin) originTable AS origins
+GROUP BY origins;
 
 -- question 2
--- same problem here, what is counted is the number of distinct combination of origins
 SELECT SIZE(origin), COUNT(prenom) FROM prenoms GROUP BY SIZE(origin);
